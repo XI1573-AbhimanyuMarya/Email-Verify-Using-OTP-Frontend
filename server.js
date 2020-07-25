aacc7e285f9d9d25569baceddece3668588702ae
Abhimanyu.Marya <abhimanyu.marya@xebia.com>
2020-07-26 05:09:00 +0530

    CHanges-to be-undone

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname + '/dist/send-email-Frontend'));
-app.get('/*', function (req, res) {
+app.get('https://verify-email-using-otp-backend.herokuapp.com/*', function (req, res) {
 
     res.sendFile(path.join(__dirname + '/dist/send-email-Frontend/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8082);
